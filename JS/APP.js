1d15f7d386f82d92cbae3c6eb5238c9bd35631c2
Marcelo Vieira <marcelo.vjunior.dev@gmail.com>
2026-05-07 17:44:03 -0300

    Atualização de sistema

--- JS/APP.js
+++ JS/APP.js
@@ -22,14 +22,31 @@ function renderCart(){
     document.getElementById("total").innerText = "Total: R$ " + total;
 }
 
-function toggleCart(forceOpen=false){
+function toggleCart(event, forceOpen=false){
+    if(event) event.preventDefault();
     const cartElement = document.getElementById("cart");
+
     if(forceOpen){
         cartElement.classList.add("active");
     }else{
         cartElement.classList.toggle("active");
     }
 }
+
+    
+document.addEventListener('DOMContentLoaded', () => {
+  const cartBar = document.getElementById("cart");
+
+  // Adiciona evento de clique na janela toda
+  window.addEventListener('click', (e) => {
+    // Evita que o clique no próprio carrinho o centralize novamente
+    if (e.target !== cartBar && !cartBar.contains(e.target)) {
+      cartBar.classList.toggle('centered');
+    }
+  });
+});
+    
+
 
 function checkout(){
     if(cart.length === 0){
@@ -43,3 +60,8 @@ function checkout(){
 function scrollToProdutos(){
     document.getElementById("produtos").scrollIntoView({behavior:"smooth"});
 }
+
+function fecharAba() {
+        if(event) event.preventDefault();
+        document.getElementById('cart').style.display = 'none';
+        }
